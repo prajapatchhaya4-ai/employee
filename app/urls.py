bc010840cca2be9e056a18f5277b65852016bbce
Chhaya prajapat <prajapatchhaya4@gmail.com>
2025-09-02 15:55:45 +0530

    first commit

---
+++ app/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('',views.index),
+    path('signup',views.signup,name='signup'),
+    path('signin',views.signin,name='signin'),
+    path('contact',views.contact,name='contact'),
+    path('signuppage',views.signuppage,name='signuppage'),
+    path('signinpage',views.signinpage,name='signinpage'),
+    path('userhome',views.userhome,name='userhome'),
+    path('logout',views.logout,name='logout')
+   
+   
+]
---
+++ employee/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render,redirect
+from .forms import Empform
+from .models import Employee
+
+# Create your views heref
+def add(request):
+   empform=Empform()
+   return render(request,'add.html',{'empform':empform})
+
+def insert(request):
+   if request.method=="POST":
+       empform=Empform(request.POST)
+       if empform.is_valid():
+         empform.save()
+         return render(request,'signup.html')
+       else:
+         return render(request,'add.html')
+   else:
+      return render(request,'add.html')   
+   
+def show(request):
+   employees=Employee.objects.all()
+   return render(request, 'show.html',{'employees':employees})   
+
+def edit(request,sid):
+   emp=Employee.objects.get(sid=sid)
+   return render(request,'edit.html',{'emp':emp})
+
+def update(request,sid):
+   if request.method=='POST':
+      emp=Employee.objects.get(sid=sid)
+      form=Empform(request.POST,instance=emp)
+      if form.is_valid():
+         form.save()
+         return redirect ('show')
+      else:
+         return render(request,'edit.html', {'emp':emp})
+   else:
+       return render(request,'edit.html', {'emp':emp})   
+   
+def delete(request,eid):
+      emp=Employee.objects.get(eid=eid)
+      emp.delete()
+      return redirect('add')
+
+
